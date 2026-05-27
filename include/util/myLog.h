6274d0a2ae2f918c5f1780c7b073badef322f88c
QSY <18843001105@163.com>
2026-05-27 18:39:57 +0800

    spdlog日志类封装完毕

--- include/util/myLog.h
+++ include/util/myLog.h
@@ -1,23 +1,35 @@
 #include <iostream>
+// 核心头文件，包含日治其，级别，全局设置
 #include <spdlog/spdlog.h>
 #include <sys/types.h>
 #include <memory>
 #include <string>
 
-namespace Log{
-    struct Lagger{
-        private:
-            void InitLagger(const spdlog::level::level_enum &grade, const std::string &savepath, const std::string &format);
+namespace Log
+{
+    // 宏： #define 名字(参数列表) 实际替换内容
+    // 对应：TRAC就是这个宏的名字，format就是一个占位符，并不是已存在的变量，...表示可变参数，就是除了format之外，调用者还能够传输 >= 0个其他参数
+    // 可变参数在替换体中用__VA_ARGS__来进行访问， ##是防止__VA_ARGS__为空从而多出来一个都好导致语法错误
+    // __FILE__	当前源文件名字符串，如 "main.cpp"
+    // __LINE__ 当前行号的十进制整数 整型常量
+    // __FUNCTION__ 或 __func__ 当前函数名字符串
+    // std::string("[{:>10s}:{:<4d}]") C风格的占位符统一格式
+#define TRACE(format, ...) Log::Logger::getLogger()->trace(std::string("[{:>10s}:{:<4d}]") + format, __FILE__, __LINE__, ##__VA_ARGS__)
 
-        private:
-            Lagger();
+    struct Lagger
+    {
+    private:
+        // 日志等级 ， 日志存储路径， 日志名称
+        static void InitLagger(const spdlog::level::level_enum &logLevel = spdlog::level::info, const std::string &logFile, const std::string &logName);
+        static std::shared_ptr<spdlog::logger> getLogger();
 
-            Lagger(const Lagger&) = delete;
+    private:
+        Lagger();
+        Lagger(const Lagger &) = delete;
+        Lagger &operator=(const Lagger &) = delete;
 
-            Lagger &operator=(const Lagger &) = delete;
-
-        private:
-            static std::shared_ptr<spdlog::logger> _logger;
-            static std::mutex _mutex;
+    private:
+        static std::shared_ptr<spdlog::logger> _logger;
+        static std::mutex _mutex;
     };
-}// myLog end
+} // myLog end
--- src/util/myLog.cpp
+++ src/util/myLog.cpp
@@ -1,24 +1,53 @@
 #include "../../include/util/myLog.h"
+
+
+// 基础文件输出目标
 #include <spdlog/sinks/basic_file_sink.h>
+// 带颜色的控制台输出目标
 #include <spdlog/sinks/stdout_color_sinks.h>
+// 异步日志库
+#include <spdlog/async.h>
 #include <functional>
 
 namespace Log{
-    std::shared_ptr<spdlog::logger> _logger = nullptr;
-    std::mutex _mutex;
+    std::shared_ptr<spdlog::logger> Log::_logger = nullptr;
+    std::mutex Log::_mutex;
 
     Lagger::Lagger()
     {}
 
-    void Lagger::InitLagger(const spdlog::level::level_enum &grade, const std::string &savepath, const std::string &format)
+    void Lagger::InitLagger(const spdlog::level::level_enum &logLevel, const std::string &logFile, const std::string &logName)
     {
         std::lock_guard<std::mutex> lock(_mutex);
         if(_logger == nullptr)
         {
-            // 没有初始化过 智能指针是空的
-            spdlog::flush_on(grade);
-            spdlog::init_thread_pool()
+            // 设置全局自动刷新级别，当日志等级 >= logLevel 时候，日志会被立即刷新到文件
+            // 从高至低：trace, debug, info, warn, error, critical, off
+            spdlog::flush_on(logLevel);
+            // 启用异步日志，将日志信息存放在队列中，由后台线程进行写入，队列大小后台进程数量，1个一般就够，太多会导致磁盘资源的竞争
+            spdlog::init_thread_pool(32768, 1);
+            if("stdcout" == logFile)
+            {
+                // 创建一个输出到控制台带颜色的日志器
+                _logger = spdlog::stdout_color_st(logName);
+            }
+            else
+            {
+                // 创建一个文件输出的日志器，日志回呗写入到指定文件中
+                _logger = spdlog::basic_logger_mt<spdlog::async_logger>(logName, logFile);
+            }
 
+            // 设置日志格式
+            // [%H:%M:%S]:时分秒
+            // [%n]：日志名称
+            // [%-7l]：日志等级  %v：日志内容
+            _logger->set_pattern("[%H:%M:%S][%n][%-7l]:%v");
+            _logger->set_level(logLevel);
         }
     }
-}
+
+    std::shared_ptr<spdlog::logger> Log::getLogger()
+    {
+        return _logger;
+    }
+}// myLog.cpp end
